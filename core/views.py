e2438f9141e891c17e3fa685a9595992229f3c45
Alejandro Exequiel Hernández Lara <alejandro@pckz.cl>
2018-12-26 13:54:21 -0300

    Implementando template y agregando a setting la app agregada

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Mi web personal</h1><h2>Portada</h2>")
+    return render(request, 'core/home.html')
 
 def about(request):
     return HttpResponse("<h1>Mi web personal</h1><h2>Acerca de</h2>")
