fd80b847b4f9b156bc084d5c85b6a17a903768fb
Alejandro Exequiel Hernández Lara <alejandro@pckz.cl>
2018-12-26 17:27:17 -0300

    Perfeccionando el uso de Modelo Vista Template

--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from django.contrib import admin
-from core import views
+from core import views as core_views
+from portfolio import views as portfolio_views
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('about/', views.about, name="about"),
-    path('contact/', views.contact, name="contact"),
-    path('portfolio/', views.portfolio, name="portfolio"),
+    path('', core_views.home, name="home"),
+    path('about/', core_views.about, name="about"),
+    path('contact/', core_views.contact, name="contact"),
+    path('portfolio/', portfolio_views.portfolio, name="portfolio"),
     path('admin/', admin.site.urls),
 ]
 
